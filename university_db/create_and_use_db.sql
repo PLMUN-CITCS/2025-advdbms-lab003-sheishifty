116a2f32cc591eb62e71e0cdbdb046fc8431b634
Shifty Shei I. Banogbanog <banogbanogshifty_bsit@plmun.edu.ph>
2025-03-09 20:37:13 +0800

    Added SQL scripts for creating and dropping UniversityDB

---
+++ university_db/create_and_use_db.sql
@@ -0,0 +1,9 @@
+-- Step 1: Create a database called UniversityDB
+CREATE DATABASE `UniversityDB`;
+
+-- Step 2: Use the newly created database
+USE `UniversityDB`;
+
+-- Verification (Optional): Check if the database is selected
+SELECT DATABASE();
+
