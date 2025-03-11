4b53e079d014a68dcdacbc2801d910fde6f74e09
Shifty Shei I. Banogbanog <banogbanogshifty_bsit@plmun.edu.ph>
2025-03-11 08:49:22 +0800

    Fixed SQL syntax issues in drop_db.sql

--- university_db/drop_db.sql
+++ university_db/drop_db.sql
@@ -1,6 +1,2 @@
--- Step 3: Drop the database if needed
--- Be cautious! This is irreversible.
-USE `UniversityDB`;
 
--- The IF EXISTS clause prevents errors if the database doesn't exist
 DROP DATABASE IF EXISTS `UniversityDB`;
