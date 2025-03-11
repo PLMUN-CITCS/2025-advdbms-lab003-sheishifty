1cac1cdb9ced4e0a3207da562d2dc9e97a71ab1b
Shifty Shei I. Banogbanog <banogbanogshifty_bsit@plmun.edu.ph>
2025-03-11 08:42:47 +0800

    Fixed database creation and deletion scripts

--- university_db/create_and_use_db.sql
+++ university_db/create_and_use_db.sql
@@ -1,5 +1,8 @@
 -- Step 1: Create a database called UniversityDB
-CREATE DATABASE `UniversityDB`;
+
+
+CREATE DATABASE IF NOT EXISTS UniversityDB;
+
 
 -- Step 2: Use the newly created database
 USE `UniversityDB`;
